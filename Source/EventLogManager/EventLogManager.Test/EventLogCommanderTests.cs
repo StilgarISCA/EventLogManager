8c7074e65262b5591eb626200120107c567d3f42
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2014-08-04 08:42:30 -0400

    Passes first TDD test

---
+++ Source/EventLogManager/EventLogManager.Test/EventLogCommanderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using EventLogManagerString = EventLogManager.ResponseString;
+
+namespace EventLogManager.Test
+{
+   public class EventLogCommanderTests : IDisposable
+   {
+      [Fact]
+      public void InvokedWithZeroArguments_ReturnsUsageString()
+      {
+         // Arrange
+         string returnValue = string.Empty;
+         string[] emptyArgumentArray = { };
+
+         // Act
+         returnValue = EventLogCommander.ProcessCommand( emptyArgumentArray );
+
+         // Assert
+         Assert.Equal( EventLogManagerString.UseageStatement, returnValue );
+      }
+
+      public void Dispose()
+      {
+      }
+   }
+}
---
+++ Source/EventLogManager/EventLogManager/EventLogCommander.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace EventLogManager
+{
+   [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses" )]
+   public static class EventLogCommander
+   {
+      public static string ProcessCommand( string[] args )
+      {
+         if( args == null || args.Length <= 0 )
+         {
+            return ResponseString.UseageStatement;
+         }
+
+         throw new NotImplementedException();
+      }
+   }
+}
