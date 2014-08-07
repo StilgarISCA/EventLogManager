972e22bada377b11b8af88703373a789698aec36
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2014-08-07 15:01:42 -0400

    List works

--- Source/EventLogManager/EventLogConnection/EventLogConnection.cs
+++ Source/EventLogManager/EventLogConnection/EventLogConnection.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace EventLogManager.Connection
 {
+   /// <summary>
+   /// Encapsulate logic for managing Windows Event Logs
+   /// </summary>
    public class EventLogConnection : IEventLogConnection
    {
+      /// <summary>
+      /// Gets a collection of event log names
+      /// </summary>
+      /// <returns>Collection of event log names</returns>
       public Collection<string> GetEventLogs()
       {
-         throw new System.NotImplementedException();
+         // TODO: Handle SystemException which can be thrown by EventLog.GetEventLogs()
+         // http://msdn.microsoft.com/en-us/library/74e2ybbs(v=vs.110).aspx
+
+         var eventLogNames = new Collection<string>();
+         EventLog[] eventLogs = EventLog.GetEventLogs();
+
+         foreach( EventLog eventLog in eventLogs )
+         {
+            eventLogNames.Add( eventLog.Log );
+         }
+
+         return eventLogNames;
       }
+
+
    }
 }
